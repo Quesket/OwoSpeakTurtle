6076a614af9ff94accc274f90c2064f0d01244a6
Radim Šimeček <radikmajz90@gmail.com>
2025-09-18 21:35:10 +0200

    Downgrading
    Downgrading to the simplest version possible that at the very least works.

---
+++ OwoSpeakTurtle.lua
@@ -0,0 +1,44 @@
+local owos = { 
+    "OwO", "owo", "UwU", "uwu", "^w^", "(´•ω•)"
+}
+
+local function tlength(t)
+    local count = 0
+    for _ in pairs(t) do
+        count = count + 1
+    end
+    return count
+end
+
+local string_gsub = string.gsub
+local makeowo = SendChatMessage
+
+function SendChatMessage(msg, chatType, language, channel, target)
+    local owo = owos[math.random(tlength(owos))]
+    local whatsthis = math.random(10)
+
+    local s = msg
+    s = string_gsub(s, "[LR]", "W")
+    s = string_gsub(s, "[lr]", "w")
+    s = string_gsub(s, "ith", "if")
+
+    if whatsthis <= 7 then
+        s = string_gsub(s, " ([fps])([aeiou])", " %1w%2")
+    end
+
+    if whatsthis <= 5 then
+        s = string_gsub(s, " n([aeiou])", " ny%1")
+    end
+
+    if whatsthis <= 3 then
+        s = string_gsub(s, " th", " d")
+    end
+
+    if whatsthis == 1 then
+        s = s .. " " .. owo
+    else
+        s = string_gsub(s, "!$", " " .. owo)
+    end
+
+    return makeowo(s, chatType, language, channel, target)
+end
